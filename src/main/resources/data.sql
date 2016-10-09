6536e26703956b39eba85f0b4151b1623abb3d42
intplus <intplus2@gmail.com>
2016-10-09 17:41:54 +0300

    data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,4 @@
-INSERT INTO user_sport VALUES ('1', '1');
-
 INSERT INTO sports VALUES ('1', 'Ukraine', 'Swimm');
-
-INSERT INTO user_role VALUES ('1', '1');
 
 INSERT INTO user VALUES ('1','Kyiv', 'Ukraine', 'Kyiv', 'kyv@gmail.com', 'Petrov', 'petrov', 'Ivan', '123', '345');
 
@@ -12,7 +8,6 @@ INSERT INTO role VALUES ('2','user', 'user');
 
 INSERT INTO role VALUES ('1','manager', 'manager');
 
-INSERT INTO role VALUES ('manager', 'manager');
+INSERT INTO user_role VALUES ('1', '1');
 
-INSERT INTO role VALUES ('', 'manager');
-
+INSERT INTO user_sport VALUES ('1', '1');
