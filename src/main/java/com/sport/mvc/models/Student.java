e755e93f2a8965b844093e58190709b79069c89f
bjj <artyrgetman@gmail.com>
2016-10-19 14:11:11 +0300

    -

--- src/main/java/com/sport/mvc/models/Student.java
+++ src/main/java/com/sport/mvc/models/Student.java
@@ -1,12 +1,8 @@
 package com.sport.mvc.models;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.util.*;
 
 @Entity
