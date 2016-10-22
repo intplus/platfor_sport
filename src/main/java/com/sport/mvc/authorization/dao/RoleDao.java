ab6b10c02c38df865bb164947c960990cd2bb859
MIxas <verlamov.m@gmail.com>
2016-10-22 23:52:10 +0300

    Merge branch 'test' of https://github.com/ArtyrGetman/platfor_sport into verlamov
    # Conflicts:
    #	pom.xml
    #	src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml

--- src/main/java/com/sport/mvc/authorization/dao/RoleDao.java
+++
@@ -1,8 +0,0 @@
-package com.sport.mvc.authorization.dao;
-
-
-import com.sport.mvc.authorization.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleDao extends JpaRepository<Role, Long> {
-}
--- src/main/java/com/sport/mvc/authorization/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.sport.mvc.authorization.dao;
-
-import com.sport.mvc.authorization.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-
-}
