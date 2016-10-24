cafc09866f69f67458b9008700229fecf620638e
bjj <artyrgetman@gmail.com>
2016-10-24 17:26:08 +0300

    new group -add href and id

--- src/main/webapp/resources/script/js.js
+++ src/main/webapp/resources/script/js.js
@@ -1,20 +1,24 @@
-function Add(){
-
+function Add(id){
 
 	var name= prompt("write tittle of group" ,"write here");
-
 	//alert("you name "+ name);
-
-
 	var newGroup = document.getElementsByTagName("ul")[1]
 	// var newUl = document.createElement("ui");
 	var newLi = document.createElement("li");
 	var a = document.createElement("a");
 	a.setAttribute('href', "http://facebook.com");
+	a.setAttribute('id', name);
 //	a.appendChild(document.setProperty("in",name)).href="http://localhost:8080/registerPerson//showFormForAdd";
 	a.appendChild(document.createTextNode(name));
 	newLi.appendChild(a);
 	newGroup.appendChild(newLi)
+
+
+}
+
+
+function save() {
+
 
 
 
