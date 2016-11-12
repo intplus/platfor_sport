aa93854ce7b7f9b4887bd12636380b8126e20a4f
bjj <artyrgetman@gmail.com>
2016-11-12 10:11:32 +0200

    send meil? some things

--- src/main/java/com/sport/mvc/models/ListDate.java
+++ src/main/java/com/sport/mvc/models/ListDate.java
@@ -1,14 +1,22 @@
 package com.sport.mvc.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "list_date")
 public class ListDate extends Model{
 
     private static final long serialVersionUID = 2333403738240612177L;
+
+    @Column
+    private Set<Integer> dateList =  new HashSet<>();
+
 
     public ListDate() {
         super();
