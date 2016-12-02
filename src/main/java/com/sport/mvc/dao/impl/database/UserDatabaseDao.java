7edf59a06890ec3810bbfb9e37d7b7dfa92a67fb
intplus <intplus2@gmail.com>
2016-12-02 22:29:13 +0200

    save

--- src/main/java/com/sport/mvc/dao/impl/database/UserDatabaseDao.java
+++ src/main/java/com/sport/mvc/dao/impl/database/UserDatabaseDao.java
@@ -3,7 +3,6 @@ package com.sport.mvc.dao.impl.database;
 import com.sport.mvc.dao.UserDao;
 
 import com.sport.mvc.models.User;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
